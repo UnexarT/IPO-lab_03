3c3b80090d4e953675b893d041a5989321c43d79
Daniil <unexart4132@gmail.com>
2024-09-15 21:40:15 +0500

    new commit

--- BubbleSort.py
+++ BubbleSort.py
@@ -1,11 +1,26 @@
 def inputMas(num):
-    return [int(input("Введите число №"+str(i)+" массива:")) for i in range(num)]
+    return [int(input("Введите число №"+str(i)+" массива: ")) for i in range(num)]
 
-def bubleSortUP(mas):
+def inputType():
+    flag = True
+    while flag:
+        type = input("Введите тип сортировки.\n"+
+                     "По возрастанию - 1\n"+
+                     "По убыванию - 2\n"
+                     "Ваш выбор: ")
+        if type.isdigit():
+            if int(type) == 1 or int(type) == 2:
+                flag = False
+    return int(type)
+
+def bubleSort(mas, type):
     for j in range(len(mas)):
-        for i in range(j,len(mas)-1):
-            if mas[i] >= mas[i+1]:
-                mas[i], mas[i+1] = mas[i+1], mas[i]
-    print(mas)
+        for i in range(j, len(mas) - 1):
+            if mas[i] >= mas[i + 1]:
+                mas[i], mas[i + 1] = mas[i + 1], mas[i]
+    if type == 1:
+        print(mas)
+    else:
+        print(mas[::-1])
 
-bubleSortUP(inputMas(5))
+bubleSort(inputMas(5),inputType())
